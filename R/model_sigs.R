db2f8eee1ff17066c982b7628f0b1d7c87a07aa1
JARS3N <JARS3N@users.noreply.github.com>
2023-04-12 13:07:34 -0400

    Update model_sigs.R

--- R/model_sigs.R
+++ R/model_sigs.R
@@ -1,7 +1,5 @@
 model_sigs<-function(mod){
-  df<-as.data.frame(cbind(signif(summary(mod)$parameters,digits=5),
-                          signif(confint(mod),digits=4)))
-  df$attr<-row.names(df)
-  row.names(df)<-NULL
-  df
+  as.data.frame(cbind(signif(summary(mod)$parameters,digits=5),
+                          signif(confint(mod),digits=4))) %>%
+  tibble::remove_rownames()
 }
